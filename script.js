81bd5549cd129c788427c7c33deffbef9a977b5c
anuragprajapati4153 <prajapatianurag4153@gmail.com>
2025-01-12 10:08:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let todoInput = document.getElementById("task-input")
+let addTaskButton = document.getElementById("add-task-btn")
+let todoList = document.getElementById("todo-list")
+
+
+let tasks = []
+addTaskButton.addEventListener("click", function () {
+    const taskText = todoInput.value.trim()
+    if (taskText == "") return;
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        completed: false
+    }
+    tasks.push(newTask)
+    todoInput.value = ""
+    console.log(tasks)
+
+})
